de12445d433254d9f3e3cb03406aabf730d420e0
Wes Lowe <wesley.lowe@yahoo.com>
2024-10-17 20:37:17 -0500

    added env validation

--- next.config.cjs
+++ next.config.cjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/lib/env.js");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
