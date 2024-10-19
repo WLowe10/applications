17e9826669137ce43b27cb5b6fd0f02b0230de6f
Wes Lowe <wesley.lowe@yahoo.com>
2024-10-19 10:10:24 -0500

    updated /lib/ai.ts

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -7,5 +7,6 @@ export async function getEmbedding(text: string) {
 		encoding_format: "float",
 	});
 
+	// i believe the response.data array always has a single element
 	return response.data[0]!.embedding;
 }
